a83119b171463820426e4e5fe3dcc8bdb5f834eb
donjoe <giumontanaro@gmail.com>
2020-03-29 22:28:42 +0200

    Add build folder

---
+++ build/precache-manifest.c133c98605378d2a643002e71ab48a32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66983e4a8bc93783a00f735badfdc931",
+    "url": "/index.html"
+  },
+  {
+    "revision": "690551ad8a6a400c2ffa",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c5cf8bd149580aa910cd",
+    "url": "/static/js/2.57bf6790.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.57bf6790.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "690551ad8a6a400c2ffa",
+    "url": "/static/js/main.36f0c9f8.chunk.js"
+  },
+  {
+    "revision": "9d2b4f7262278411ffe7",
+    "url": "/static/js/runtime-main.4464e187.js"
+  }
+]);
